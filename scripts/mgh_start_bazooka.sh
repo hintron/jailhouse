e23ba47a230bd8774e92e5f9d1b957df50668118
Michael Hinton <hinton@schedmd.com>
2019-02-08 20:07:47 -0700

    Update helper scripts

--- scripts/mgh_start_bazooka.sh
+++ scripts/mgh_start_bazooka.sh
@@ -1,4 +1,3 @@
-echo "MGH: Starting Bazooka cell"
 ./tools/jailhouse cell create configs/x86/bazooka-demo.cell
 ./tools/jailhouse cell load bazooka-demo inmates/demos/x86/bazooka-demo.bin
 ./tools/jailhouse cell start bazooka-demo
