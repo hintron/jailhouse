9f8798519faef3c4b5f98bcf207f0815c14a5e07
Michael Hinton <hinton@schedmd.com>
2019-02-27 20:27:13 -0700

    Create MGH copies of qemu x86 root and x86 ivshmem inmate
    Create and rename scripts.

---
+++ scripts/end_mgh.sh
@@ -0,0 +1,1 @@
+./tools/jailhouse disable
---
+++ scripts/start_mgh_inmate.sh
@@ -0,0 +1,3 @@
+./tools/jailhouse cell create configs/x86/mgh-demo.cell
+./tools/jailhouse cell load mgh-demo inmates/demos/x86/mgh-demo.bin
+./tools/jailhouse cell start mgh-demo
