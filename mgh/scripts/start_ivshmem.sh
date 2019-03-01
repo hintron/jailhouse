dc2abd376848efef216c197446d28e1da29bb2f8
Michael Hinton <hinton@schedmd.com>
2019-02-28 20:15:42 -0700

    Fix broken paths

--- mgh/scripts/start_ivshmem.sh
+++ mgh/scripts/start_ivshmem.sh
@@ -1,4 +1,4 @@
 cd "${BASH_SOURCE%/*}" || exit
-../../tools/jailhouse cell create configs/x86/ivshmem-demo.cell
-../../tools/jailhouse cell load ivshmem-demo inmates/demos/x86/ivshmem-demo.bin
+../../tools/jailhouse cell create ../../configs/x86/ivshmem-demo.cell
+../../tools/jailhouse cell load ivshmem-demo ../../inmates/demos/x86/ivshmem-demo.bin
 ../../tools/jailhouse cell start ivshmem-demo
