2f1551d60eb048b5180edabf0fee25031795dd29
Michael Hinton <hinton@schedmd.com>
2019-03-05 16:20:55 -0700

    Fix path issue

--- mgh/scripts/start_mgh_root.sh
+++ mgh/scripts/start_mgh_root.sh
@@ -1,4 +1,4 @@
 cd "${BASH_SOURCE%/*}" || exit
-VERSION=$(./tools/jailhouse --version)
+VERSION=$(../../tools/jailhouse --version)
 echo "MGH: Starting $VERSION in QEMU"
 ../../tools/jailhouse enable ../../configs/x86/qemu-mgh.cell
