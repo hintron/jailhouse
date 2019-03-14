1a47680c87784f8d9c5120d5de630c934b3990a9
Michael Hinton <hinton@schedmd.com>
2019-03-14 17:08:39 -0600

    Fix compile errors

--- hypervisor/include/jailhouse/printk.h
+++ hypervisor/include/jailhouse/printk.h
@@ -30,3 +30,15 @@ extern void (*arch_dbg_write)(const char *msg);
 
 extern bool virtual_console;
 extern volatile struct jailhouse_virt_console console;
+
+// MGH: Add debug print macro if DEBUG is defined (pass in to compiler)
+#ifdef DEBUG
+#define MGH_DEBUG 1
+#else
+#define MGH_DEBUG 0
+#endif
+#define printmgh(fmt, ...) \
+    do { if (MGH_DEBUG) printk(fmt, ##__VA_ARGS__); } while (0)
+
+// See https://stackoverflow.com/questions/1644868/define-macro-for-debug-printing-in-c
+// https://stackoverflow.com/questions/18968070/error-when-defining-a-stringising-macro-with-va-args
