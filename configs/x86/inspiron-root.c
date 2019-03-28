53aefa49ea8dbf94bdd220d742c3192777cc407a
Michael Hinton <hinton@schedmd.com>
2019-03-27 22:06:21 -0600

    Re-add ivshmem device not that root cell is working

--- configs/x86/inspiron-root.c
+++ configs/x86/inspiron-root.c
@@ -47,14 +47,12 @@
 struct {
 	struct jailhouse_system header;
 	__u64 cpus[1];
-	struct jailhouse_memory mem_regions[61];
 	// MGH: Increment by 1 to 62 after adding ivshmem mem region
-	// struct jailhouse_memory mem_regions[62];
+	struct jailhouse_memory mem_regions[62];
 	struct jailhouse_irqchip irqchips[1];
 	__u8 pio_bitmap[0x2000];
-	struct jailhouse_pci_device pci_devices[16];
 	// MGH: Increment by 1 to 17 after adding ivshmem pci device
-	// struct jailhouse_pci_device pci_devices[17];
+	struct jailhouse_pci_device pci_devices[17];
 	struct jailhouse_pci_capability pci_caps[40];
 } __attribute__((packed)) config = {
 	.header = {
@@ -551,14 +549,14 @@ struct {
 			// .size = 0x4c00000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE,
 		},
-		// /* MGH Added: IVSHMEM shared memory region (index 61)*/
-		// {
-		// 	.phys_start = 0x3f101000,
-		// 	.virt_start = 0x3f101000,
-		// 	// Create 1 MB of shared memory
-		// 	.size = 0xff000,
-		// 	.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE,
-		// },
+		/* MGH Added: IVSHMEM shared memory region (index 61)*/
+		{
+			.phys_start = 0x3f101000,
+			.virt_start = 0x3f101000,
+			// Create 1 MB of shared memory
+			.size = 0xff000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE,
+		},
 	},
 
 	.irqchips = {
@@ -880,20 +878,20 @@ struct {
 			.msix_region_size = 0x0,
 			.msix_address = 0x0,
 		},
-		// /* MGH Added: IVSHMEM (demo) */
-		// {
-		// 	.type = JAILHOUSE_PCI_TYPE_IVSHMEM,
-		// 	.domain = 0x0000,
-		// 	.bdf = 0x0f << 3,
-		// 	.bar_mask = {
-		// 		0xffffff00, 0xffffffff, 0x00000000,
-		// 		0x00000000, 0xffffffe0, 0xffffffff,
-		// 	},
-		// 	.num_msix_vectors = 1,
-		// 	.shmem_region = 61,
-		// 	.shmem_protocol = JAILHOUSE_SHMEM_PROTO_UNDEFINED,
-		// 	// MGH: .iommu=1 seems to break things
-		// },
+		/* MGH Added: IVSHMEM (demo) */
+		{
+			.type = JAILHOUSE_PCI_TYPE_IVSHMEM,
+			.domain = 0x0000,
+			.bdf = 0x0f << 3,
+			.bar_mask = {
+				0xffffff00, 0xffffffff, 0x00000000,
+				0x00000000, 0xffffffe0, 0xffffffff,
+			},
+			.num_msix_vectors = 1,
+			.shmem_region = 61,
+			.shmem_protocol = JAILHOUSE_SHMEM_PROTO_UNDEFINED,
+			// MGH: .iommu=1 seems to break things
+		},
 	},
 
 	.pci_caps = {
