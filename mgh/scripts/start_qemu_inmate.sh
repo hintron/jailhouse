114c326724163cf0c0f32eb2c63aeec7a8960df9
Michael Hinton <hinton@schedmd.com>
2019-05-08 23:18:47 -0600

    Update convenience scripts

--- mgh/scripts/start_qemu_inmate.sh
+++ mgh/scripts/start_qemu_inmate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "${BASH_SOURCE%/*}" || exit
-../../tools/jailhouse cell create ../../configs/x86/mgh-demo.cell
-../../tools/jailhouse cell load mgh-demo ../../inmates/demos/x86/mgh-demo.bin
-../../tools/jailhouse cell start mgh-demo
+sudo ../../tools/jailhouse cell create ../../configs/x86/mgh-demo.cell
+sudo ../../tools/jailhouse cell load mgh-demo ../../inmates/demos/x86/mgh-demo.bin
+sudo ../../tools/jailhouse cell start mgh-demo
--- mgh/scripts/start_qemu_root.sh
+++ mgh/scripts/start_qemu_root.sh
@@ -2,4 +2,4 @@
 cd "${BASH_SOURCE%/*}" || exit
 VERSION=$(../../tools/jailhouse --version)
 echo "MGH: Starting $VERSION in QEMU"
-../../tools/jailhouse enable ../../configs/x86/qemu-mgh.cell
+sudo ../../tools/jailhouse enable ../../configs/x86/qemu-mgh.cell
