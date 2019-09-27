fe597b37ec4882ee15a50dc2bf020d53eccf4ae9
Michael Hinton <michael.g.hinton@gmail.com>
2019-09-26 23:17:15 -0600

    Create a profile program that runs many iterations before printing
    For Vtune

---
+++ mgh/sha3/src/profile_main.c
@@ -0,0 +1,42 @@
+#include "include/sha3_mgh.h"
+#include <stdio.h>
+#include <string.h>
+
+// A 512-bit hash is really 64 bytes x 8 bits/byte
+#define MD_LENGTH 64
+#define ITERATIONS 6
+#define ITERATIONS_REAL ((ITERATIONS-2)/2)
+int main(int argc, char const *argv[])
+{
+	char buffer_a[MD_LENGTH] = {0};
+	char buffer_b[MD_LENGTH] = {0};
+
+	// Copy the initial input seed string
+	strcpy(buffer_a, "testing");
+
+	(void) sha3_mgh(buffer_a, strlen(buffer_a), &buffer_b, MD_LENGTH);
+	// for (int i = 0; i < MD_LENGTH; ++i) {
+	// 	printf("%02hhx", buffer_b[i]);
+	// }
+	// printf("\n");
+	for (int i = 0; i < ITERATIONS_REAL; ++i) {
+		(void) sha3_mgh(buffer_b, MD_LENGTH, &buffer_a, MD_LENGTH);
+		// for (int i = 0; i < MD_LENGTH; ++i) {
+		// 	printf("%02hhx", buffer_a[i]);
+		// }
+		// printf("\n");
+		(void) sha3_mgh(buffer_a, MD_LENGTH, &buffer_b, MD_LENGTH);
+		// for (int i = 0; i < MD_LENGTH; ++i) {
+		// 	printf("%02hhx", buffer_b[i]);
+		// }
+		// printf("\n");
+	}
+	(void) sha3_mgh(buffer_b, MD_LENGTH, &buffer_a, MD_LENGTH);
+
+	for (int i = 0; i < MD_LENGTH; ++i) {
+		printf("%02hhx", buffer_a[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
---
+++ mgh/sha3/test-profile.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+cd "${BASH_SOURCE%/*}" || exit
+
+# Compare my borrowed sha3 code to a 3rd-party tool (RHash)
+
+# Test the locally-built binary instead of what's on the path
+mgh_bin=./build/profile-sha3-512
+
+if hash rhash 2>/dev/null; then
+    echo "rhash found"
+else
+    printf "rhash not found. Install it:\n\n    sudo apt install rhash\n\n"
+    exit
+fi
+
+if [ -f $mgh_bin ] ; then
+    echo "$mgh_bin found"
+else
+    printf "$mgh_bin not found. Build it with Meson:\n\n    meson build\n    ninja -C build\n\n"
+    exit
+fi
+
+
+
+function rhash_sha3_512_hex {
+    # Remove trailing characters from rhash output
+    trailing=" (stdin)"
+    out="$(printf "$1" | xxd -r -p | rhash --sha3-512 -)"
+    out=${out%$trailing}
+    echo $out
+}
+
+
+function rhash_sha3_512 {
+    # Remove trailing characters from rhash output
+    trailing=" (stdin)"
+    out="$(printf "$1" | rhash --sha3-512 -)"
+    out=${out%$trailing}
+    echo $out
+}
+
+ITERATIONS=6
+ITERATIONS_REAL=$((($ITERATIONS-2) / 2))
+
+# echo $ITERATIONS_REAL
+
+RHASH_B=$(rhash_sha3_512 "testing")
+RHASH_A=""
+for ((i = 0; i < $ITERATIONS_REAL; i++)); do
+    RHASH_A=$(rhash_sha3_512_hex "$RHASH_B")
+    RHASH_B=$(rhash_sha3_512_hex "$RHASH_A")
+done
+RHASH_A=$(rhash_sha3_512_hex "$RHASH_B")
+
+MGH_OUT=$($mgh_bin)
+
+if [ "$RHASH_A" != "$MGH_OUT" ]; then
+    echo "FAILURE: rhash and mgh differ!:"
+    echo "    rhash: $RHASH_A"
+    echo "    mgh  : $MGH_OUT"
+    # Assume we are the ones to blame
+    MGH_FAILED="FAILED"
+else
+    echo "rhash and mgh match"
+fi
