ce26df23577e39a9393ff467f41c14ce196734e6
Michael Hinton <michael.g.hinton@gmail.com>
2019-10-26 16:10:25 -0600

    Force make to use GCC 7 to avoid broken cell configs
    GCC 8 and 9 seem to cause the cell configuration to compile wrong.
    https://groups.google.com/d/topic/jailhouse-dev/ynfUHrI3bkQ/discussion

--- mgh/scripts/build.sh
+++ mgh/scripts/build.sh
@@ -2,8 +2,8 @@
 cd "${BASH_SOURCE%/*}" || exit
 
 cd ../..
-make > /dev/null
-sudo make install > /dev/null
+make CC=gcc-7 > /dev/null
+sudo make install CC=gcc-7 > /dev/null
 cd mgh/uio-kernel-module
 make > /dev/null
 sudo make install > /dev/null
