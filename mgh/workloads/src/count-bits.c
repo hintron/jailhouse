257fff1a39e00c273804f235272bfac557dd906e
Michael Hinton <michael.g.hinton@gmail.com>
2019-11-06 23:05:35 -0700

    Create three separate methods for counting bits
    Method 0 (LUT) is the fastest by far. Methods 0 and 1 are about the
    same.

--- mgh/workloads/src/count-bits.c
+++ mgh/workloads/src/count-bits.c
@@ -7,8 +7,84 @@
 
 #define MAX_INPUT_PRINT 50
 
-static int count_set_bits(unsigned char *input,
-			  int input_len)
+int lut [256] = {
+	// Pattern for first 3 bits
+	// Add 1 for all bits set in upper 5 bits
+
+	// 00000000 -> 0 (first value in row)
+	// 00000001
+	// 00000010
+	// 00000011
+	// 00000100
+	// 00000101
+	// 00000110
+	// 00000111
+	0, 1, 1, 2, 1, 2, 2, 3,
+	// 00001000 -> 1
+	1, 2, 2, 3, 2, 3, 3, 4,
+	// 00010000 -> 1
+	1, 2, 2, 3, 2, 3, 3, 4,
+	// 00011000 -> 2
+	2, 3, 3, 4, 3, 4, 4, 5,
+	// 00100000 -> 1
+	1, 2, 2, 3, 2, 3, 3, 4,
+	// 00101000 -> 2
+	2, 3, 3, 4, 3, 4, 4, 5,
+	// 00110000 -> 2
+	2, 3, 3, 4, 3, 4, 4, 5,
+	// 00111000 -> 3
+	3, 4, 4, 5, 4, 5, 5, 6,
+	// 01000000 -> 1
+	1, 2, 2, 3, 2, 3, 3, 4,
+	// 01001000 -> 2
+	2, 3, 3, 4, 3, 4, 4, 5,
+	// 01010000 -> 2
+	2, 3, 3, 4, 3, 4, 4, 5,
+	// 01011000 -> 3
+	3, 4, 4, 5, 4, 5, 5, 6,
+	// 01100000 -> 2
+	2, 3, 3, 4, 3, 4, 4, 5,
+	// 01101000 -> 3
+	3, 4, 4, 5, 4, 5, 5, 6,
+	// 01110000 -> 3
+	3, 4, 4, 5, 4, 5, 5, 6,
+	// 01111000 -> 4
+	4, 5, 5, 6, 5, 6, 6, 7,
+	// 10000000 -> 1
+	1, 2, 2, 3, 2, 3, 3, 4,
+	// 10001000 -> 2
+	2, 3, 3, 4, 3, 4, 4, 5,
+	// 10010000 -> 2
+	2, 3, 3, 4, 3, 4, 4, 5,
+	// 10011000 -> 3
+	3, 4, 4, 5, 4, 5, 5, 6,
+	// 10100000 -> 2
+	2, 3, 3, 4, 3, 4, 4, 5,
+	// 10101000 -> 3
+	3, 4, 4, 5, 4, 5, 5, 6,
+	// 10110000 -> 3
+	3, 4, 4, 5, 4, 5, 5, 6,
+	// 10111000 -> 4
+	4, 5, 5, 6, 5, 6, 6, 7,
+	// 11000000 -> 2
+	2, 3, 3, 4, 3, 4, 4, 5,
+	// 11001000 -> 3
+	3, 4, 4, 5, 4, 5, 5, 6,
+	// 11010000 -> 3
+	3, 4, 4, 5, 4, 5, 5, 6,
+	// 11011000 -> 4
+	4, 5, 5, 6, 5, 6, 6, 7,
+	// 11100000 -> 3
+	3, 4, 4, 5, 4, 5, 5, 6,
+	// 11101000 -> 4
+	4, 5, 5, 6, 5, 6, 6, 7,
+	// 11110000 -> 4
+	4, 5, 5, 6, 5, 6, 6, 7,
+	// 11111000 -> 5
+	5, 6, 6, 7, 6, 7, 7, 8,
+};
+
+static int _count_set_bits(unsigned char *input, int input_len, int mode)
 {
 	int count = 0;
 	int print = 0;
@@ -23,16 +99,67 @@ static int count_set_bits(unsigned char *input,
 
 	// Loop through each byte
 	for (int i = 0; i < input_len; ++i) {
+		unsigned char tmp = 0;
 		if (print)
 			printf("0x%0x\n", input[i]);
+
 		// Loop through each bit
-		for (int j = 0; j < 8; ++j) {
+		switch (mode) {
+		default:
+		case 0:
+			// Fast
+			tmp = input[i];
+			for (int j = 0; j < 8; ++j) {
+				if (0x1 & tmp)
+					count++;
+				tmp >>= 1;
+			}
+			break;
+		case 1:
+			// Faster
 			if (0x1 & input[i])
 				count++;
-			input[i] >>= 1;
+			if (0x2 & input[i])
+				count++;
+			if (0x4 & input[i])
+				count++;
+			if (0x8 & input[i])
+				count++;
+			if (0x10 & input[i])
+				count++;
+			if (0x20 & input[i])
+				count++;
+			if (0x40 & input[i])
+				count++;
+			if (0x80 & input[i])
+				count++;
+			break;
+		case 2:
+			// Fastest
+			count += lut[input[i]];
+			break;
 		}
+
 	}
 	return count;
+}
+
+static int count_set_bits_0(unsigned char *input, int input_len)
+{
+	printf("%s\n", __func__);
+	return _count_set_bits(input, input_len, 0);
+}
+
+static int count_set_bits_1(unsigned char *input, int input_len)
+{
+	printf("%s\n", __func__);
+	return _count_set_bits(input, input_len, 1);
+}
+
+static int count_set_bits_2(unsigned char *input, int input_len)
+{
+	printf("%s\n", __func__);
+	return _count_set_bits(input, input_len, 2);
 }
 
 int main(int argc, char const *argv[])
@@ -41,7 +168,9 @@ int main(int argc, char const *argv[])
 	struct stat file_info;
 	int input_len = 0;
 	void *input_buffer = NULL;
-	int result = 0;
+	int result0 = 0;
+	int result1 = 0;
+	int result2 = 0;
 
 	if (argc != 2) {
 		printf("Usage:\n");
@@ -83,12 +212,24 @@ int main(int argc, char const *argv[])
 	}
 
 	// Count the number of 1's in the input_buffer's binary data and return
-	result = count_set_bits(input_buffer, input_len);
+	result0 = count_set_bits_0(input_buffer, input_len);
+	result1 = count_set_bits_1(input_buffer, input_len);
+	result2 = count_set_bits_2(input_buffer, input_len);
 
-	if (result == 1) {
+	if (result0 != result1 || result0 != result2) {
+		printf("ERROR: result0 (%d), result1 (%d), and result2 (%d) differ\n",
+		       result0, result1, result2);
+		return 1;
+	} else {
+		printf("result0, result1, and result2 all match\n");
+	}
+
+	if (result0 == 1) {
 		printf("There is 1 set bit in the input file\n");
 	} else {
-		printf("There are %d set bits in the input file\n", result);
+		printf("There are %d set bits in the input file\n", result0);
 	}
+
+	// Success
 	return 0;
 }
