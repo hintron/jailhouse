3cc6dddef2a472281e13dd8b43240bad372c6b6d
Michael Hinton <michael.g.hinton@gmail.com>
2019-11-11 18:51:01 -0700

    Improve start_experiments.sh
    Get jailhouse console output and shell output saved off to their own
    files, respectively.

--- mgh/scripts/start_experiments.sh
+++ mgh/scripts/start_experiments.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source ./common.sh
-echo "MGH: Experiments"
 ################################################################################
 # Set Jailhouse inputs here
 ################################################################################
@@ -16,22 +15,26 @@ ITERATIONS=20
 # TODO: Get a timestamp?
 experiment_time="$(timestamp)"
 INPUT_FILE="input/input_${experiment_time}.txt"
-OUTPUT_FILE="output/output_${experiment_time}.txt"
+JAILHOUSE_OUTPUT_FILE="output/jailhouse_${experiment_time}.txt"
+EXPERIMENT_OUTPUT_FILE="output/experiment_${experiment_time}.txt"
 ################################################################################
 # Experiment-wide setup
 ################################################################################
-reset_jailhouse_all
+touch $JAILHOUSE_OUTPUT_FILE
+touch $EXPERIMENT_OUTPUT_FILE
+echo "MGH: Experiments" >> $EXPERIMENT_OUTPUT_FILE
 
-touch $OUTPUT_FILE
-echo "Start time: $experiment_time" >> $OUTPUT_FILE
-echo "*******************************************************" >> $OUTPUT_FILE
+reset_jailhouse_all >> $EXPERIMENT_OUTPUT_FILE 2>&1
+
+echo "Start time: $experiment_time" >> $JAILHOUSE_OUTPUT_FILE
+echo "*******************************************************" >> $JAILHOUSE_OUTPUT_FILE
 
 # Start recording experiment output
 # Put process in the background and kill it once done
-sudo jailhouse console -f >> $OUTPUT_FILE &
+sudo jailhouse console -f >> $JAILHOUSE_OUTPUT_FILE &
 tailf_pid=$!
 
-echo "tailf_pid: $tailf_pid"
+echo "tailf_pid: $tailf_pid" >> $EXPERIMENT_OUTPUT_FILE
 
 ################################################################################
 # Experiment 1
@@ -45,65 +48,45 @@ echo "tailf_pid: $tailf_pid"
 # COUNT_SET_BITS_MODE=$CSBM_FASTEST
 # POLLUTE_CACHE="true"
 # Generate command line arguments based on input
-INMATE_CMDLINE=$(set_cmdline)
+INMATE_CMDLINE=$(set_cmdline) >> $EXPERIMENT_OUTPUT_FILE 2>&1
 ################################################################################
 
-end_jailhouse
-echo "*******************************************************" >> $OUTPUT_FILE
-echo "Experiment 1" >> $OUTPUT_FILE
-echo "*******************************************************" >> $OUTPUT_FILE
-start_jailhouse $ROOT_CELL $INMATE_CELL $INMATE_NAME $INMATE_PROGRAM "$INMATE_CMDLINE"
+end_jailhouse >> $EXPERIMENT_OUTPUT_FILE 2>&1
+echo "*******************************************************" >> $JAILHOUSE_OUTPUT_FILE
+echo "Experiment 1" >> $JAILHOUSE_OUTPUT_FILE
+echo "*******************************************************" >> $JAILHOUSE_OUTPUT_FILE
+start_jailhouse $ROOT_CELL $INMATE_CELL $INMATE_NAME $INMATE_PROGRAM "$INMATE_CMDLINE" >> $EXPERIMENT_OUTPUT_FILE 2>&1
 
 # Run X ITERATIONS from 1 to X
 for ((i = 0 ; i < $ITERATIONS ; i++)); do
-    echo "Iteration $i"
-    create_random_file_max $INPUT_FILE
-    send_inmate_input $INPUT_FILE
+    echo "Iteration $i" >> $EXPERIMENT_OUTPUT_FILE
+    create_random_file_max $INPUT_FILE >> $EXPERIMENT_OUTPUT_FILE 2>&1
+    send_inmate_input $INPUT_FILE >> $EXPERIMENT_OUTPUT_FILE 2>&1
 done
-
-
-echo "sudo kill $tailf_pid"
-sudo kill $tailf_pid
-
-
-
 
 # ################################################################################
 # ################################################################################
 # # Experiment 2
 # ################################################################################
-# # Inmate inputs
-# ################################################################################
-# DEBUG_MODE="true"
-# LOCAL_BUFFER="true"
-# THROTTLE_MODE=$TM_DEADLINE
-# WORKLOAD_MODE=$WM_COUNT_SET_BITS
-# COUNT_SET_BITS_MODE=$CSBM_FASTEST
-# POLLUTE_CACHE="true"
-# # Generate command line arguments based on input
-# INMATE_CMDLINE=$(set_cmdline)
-# ################################################################################
-
-# reset_jailhouse
-# start_jailhouse $ROOT_CELL $INMATE_CELL $INMATE_NAME $INMATE_PROGRAM $INMATE_CMDLINE
-
-
-
-
+#
+# TODO
+#
 # ################################################################################
 # Final Cleanup
 # ################################################################################
 
 # Flush any buffers
-echo "*******************************************************" >> $OUTPUT_FILE
-echo "End Experiments" >> $OUTPUT_FILE
-echo "*******************************************************" >> $OUTPUT_FILE
+echo "*******************************************************" >> $JAILHOUSE_OUTPUT_FILE
+echo "End Experiments" >> $JAILHOUSE_OUTPUT_FILE
+echo "*******************************************************" >> $JAILHOUSE_OUTPUT_FILE
 
-# echo "sudo rm $INPUT_FILE"
-# sudo rm $INPUT_FILE
+# The input is just random data, so really no sense in keeping it around rn
+echo "sudo rm $INPUT_FILE" >> $EXPERIMENT_OUTPUT_FILE
+sudo rm $INPUT_FILE >> $EXPERIMENT_OUTPUT_FILE 2>&1
 
-end_jailhouse_processes
+echo "sudo kill $tailf_pid" >> $EXPERIMENT_OUTPUT_FILE
+sudo kill $tailf_pid >> $EXPERIMENT_OUTPUT_FILE 2>&1
+# end_jailhouse_processes >> $EXPERIMENT_OUTPUT_FILE 2>&1
 
-
-end_inmate
-end_root
+end_inmate >> $EXPERIMENT_OUTPUT_FILE 2>&1
+end_root >> $EXPERIMENT_OUTPUT_FILE 2>&1
