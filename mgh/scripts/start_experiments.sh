773d29d871d87b9d6e9d7ccfb5ed41aeefc6abb6
Michael Hinton <michael.g.hinton@gmail.com>
2019-12-04 22:19:05 -0700

    Factor out inmate cmdline calculation

--- mgh/scripts/start_experiments.sh
+++ mgh/scripts/start_experiments.sh
@@ -76,7 +76,6 @@ function main {
     ################################################################################
     # WORKLOAD_MODE defaults to CSB
     # Generate command line arguments based on input above
-    INMATE_CMDLINE=$(set_cmdline) >> $EXPERIMENT_OUTPUT_FILE 2>&1
     ################################################################################
     start_experiment $experiment_counter
     experiment_counter=$(($experiment_counter + 1))
@@ -85,7 +84,6 @@ function main {
     # Inmate inputs
     ##################################################################################
     WORKLOAD_MODE=$WM_SHA3
-    INMATE_CMDLINE=$(set_cmdline) >> $EXPERIMENT_OUTPUT_FILE 2>&1
     ##################################################################################
     start_experiment $experiment_counter
     experiment_counter=$(($experiment_counter + 1))
@@ -94,7 +92,6 @@ function main {
     # Inmate inputs
     ##################################################################################
     WORKLOAD_MODE=$WM_RANDOM_ACCESS
-    INMATE_CMDLINE=$(set_cmdline) >> $EXPERIMENT_OUTPUT_FILE 2>&1
     ##################################################################################
     start_experiment $experiment_counter
     experiment_counter=$(($experiment_counter + 1))
@@ -138,6 +135,8 @@ function start_experiment {
     echo "################################################################################" >> $EXPERIMENT_OUTPUT_FILE
     echo "# Experiment $experiment_counter" >> $EXPERIMENT_OUTPUT_FILE
     echo "################################################################################" >> $EXPERIMENT_OUTPUT_FILE
+
+    INMATE_CMDLINE=$(set_cmdline) >> $EXPERIMENT_OUTPUT_FILE 2>&1
 
     # Start recording experiment output
     end_jailhouse >> $EXPERIMENT_OUTPUT_FILE 2>&1
