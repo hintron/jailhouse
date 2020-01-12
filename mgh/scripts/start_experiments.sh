36a2f8b819d80fb8374db8849c46459d1962bc34
Michael Hinton <michael.g.hinton@gmail.com>
2020-01-11 23:40:35 -0700

    Improve layout of start_experiments.sh

--- mgh/scripts/start_experiments.sh
+++ mgh/scripts/start_experiments.sh
@@ -6,14 +6,35 @@ source ./common.sh > /dev/null
 # $3 - RUN_ON_LINUX (default 0)
 # $4 - THROTTLE_MODE (default $TMODE_ITERATION)
 function main {
-    # ./start_experiment.sh $WM_COUNT_SET_BITS
+    local LINUX=1
+    local INMATE=0
+
+    # # Run three types of inmate workloads with interference workload
     # ./start_experiment.sh $WM_SHA3
+    # ./start_experiment.sh $WM_COUNT_SET_BITS
     # ./start_experiment.sh $WM_RANDOM_ACCESS
-    # ./start_experiment.sh $WM_RANDOM_ACCESS $INTF_NONE 1
-    # ./start_experiment.sh $WM_COUNT_SET_BITS $INTF_NONE 1
-    # ./start_experiment.sh $WM_SHA3 $INTF_NONE 0
-    # ./start_experiment.sh $WM_SHA3 $INTF_HANDBRAKE 1
-    # ./start_experiment.sh $WM_COUNT_SET_BITS $INTF_NONE 0 $TMODE_DISABLED
+
+    # # Run three types of inmate workloads without interference workload
+    # ./start_experiment.sh $WM_RANDOM_ACCESS $INTF_NONE $LINUX
+    # ./start_experiment.sh $WM_COUNT_SET_BITS $INTF_NONE $LINUX
+    # ./start_experiment.sh $WM_SHA3 $INTF_NONE $INMATE
+
+    # # Run CSB inmate workload without any throttling
+    # ./start_experiment.sh $WM_COUNT_SET_BITS $INTF_NONE $INMATE $TMODE_DISABLED
+
+    # # Run three types of Linux workloads with interference workload
+    # ./start_experiment.sh $WM_SHA3 $INTF_HANDBRAKE $LINUX
+    # ./start_experiment.sh $WM_COUNT_SET_BITS $INTF_HANDBRAKE $LINUX
+    # ./start_experiment.sh $WM_RANDOM_ACCESS $INTF_HANDBRAKE $LINUX
+
+    # # Run three types of Linux workloads without interference workload
+    # ./start_experiment.sh $WM_SHA3 $INTF_NONE $LINUX
+    # ./start_experiment.sh $WM_COUNT_SET_BITS $INTF_NONE $LINUX
+    # ./start_experiment.sh $WM_RANDOM_ACCESS $INTF_NONE $LINUX
+
+    # # Compare Linux workload to inmate workload with same inputs
+    # ./start_experiment.sh $WM_SHA3 $INTF_NONE
+    # ./start_experiment.sh $WM_SHA3 $INTF_NONE $LINUX
 }
 
 # Call main here to allow for forward declaration (like Python)
