c2c0f559b231c300faafcd362b2f42fed1832497
Michael Hinton <michael.g.hinton@gmail.com>
2020-02-04 20:02:03 -0700

    Add script to create an initramfs image

---
+++ mgh/scripts/create_initramfs.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Taken directly from https://dev.libreneitor.com/how-to-create-an-initramfs-after-you-compile-a-linux-kernel/
+
+ARCH="x86_64"
+BB_VER="1.31.0"
+
+# Dirs
+mkdir -p root
+cd root
+mkdir -p bin dev etc lib mnt proc sbin sys tmp var
+cd -
+
+# Utils
+if [ ! -f "root/bin/busybox" ]; then
+    curl -L "https://www.busybox.net/downloads/binaries/${BB_VER}-defconfig-multiarch-musl/busybox-${ARCH}" >root/bin/busybox
+fi
+cd root/bin
+chmod +x busybox
+ln -s busybox mount
+ln -s busybox sh
+cd -
+
+# Init process
+
+cat >>root/init << EOF
+#!/bin/busybox sh
+/bin/busybox --install -s /bin
+mount -t devtmpfs  devtmpfs  /dev
+mount -t proc      proc      /proc
+mount -t sysfs     sysfs     /sys
+mount -t tmpfs     tmpfs     /tmp
+setsid cttyhack sh
+exec /bin/sh
+EOF
+chmod +x root/init
+
+# initramfs creation
+
+cd root
+find . | cpio -ov --format=newc | gzip -9 >../initramfs
+cd -
