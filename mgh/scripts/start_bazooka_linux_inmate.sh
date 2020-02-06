128dd2589a637bbf18e00c7495b32c1ff8d8f5f1
Michael Hinton <michael.g.hinton@gmail.com>
2020-02-06 13:25:58 -0700

    WIP: update linux inmate start script

--- mgh/scripts/start_bazooka_linux_inmate.sh
+++ mgh/scripts/start_bazooka_linux_inmate.sh
@@ -16,10 +16,16 @@ INMATE_CELL="../../configs/x86/bazooka-inmate-linux.cell"
 # Modified Alpine Linux kernel on the Linux queue/jailhouse branch with
 # configs set to enable Jailhouse and IVSHMEM.
 # TODO: This needs to be debugged with the serial port (it crashes after starup)
-KERNEL="/home/hintron/code/linux/arch/x86/boot/bzImage-alpine-jailhouse"
+KERNEL="/home/hintron/code/linux/arch/x86/boot/bzImage-alpine-jailhouse-2"
 INITRAMFS="./images/initramfs-alpine-3.11.3.bin"
 
-sudo ../../tools/jailhouse cell linux $INMATE_CELL $KERNEL -i $INITRAMFS -c "console=ttyS0,115200"
+
+# https://www.youtube.com/watch?v=7fiJbwmhnRw&t
+# CMDLINE="console=ttyS0,115200"
+# Even though it's S4 on the root, it will probably be S0 on the inmate
+CMDLINE="console=ttyS0,115200 earlyprintk=serial,ttyS0 ip=192.168.19.2"
+
+sudo ../../tools/jailhouse cell linux $INMATE_CELL $KERNEL -i $INITRAMFS -c "$CMDLINE"
 
 # --write-params shows what the `jailhouse cell linux` command does under the
 # covers, I think
