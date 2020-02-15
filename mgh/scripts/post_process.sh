443bc803d208eff07c64752ef862bc4cb7f7f9eb
Michael Hinton <michael.g.hinton@gmail.com>
2020-02-14 17:45:10 -0700

    Allow paths to still work w/ post processing after folder rename
    Jailhouse output and vtune output are the vital ones for post
    processing.

--- mgh/scripts/post_process.sh
+++ mgh/scripts/post_process.sh
@@ -31,15 +31,18 @@ INPUT_SIZE_END=$(grep_token_in_file "INPUT_SIZE_END: " $data_output_dir/experime
 INPUT_SIZE_STEP=$(grep_token_in_file "INPUT_SIZE_STEP: " $data_output_dir/experiment_*.txt)
 INPUT_FILE=$(grep_token_in_file "INPUT_FILE: " $data_output_dir/experiment_*.txt)
 INTERFERENCE_WORKLOAD=$(grep_token_in_file "INTERFERENCE_WORKLOAD_RAW: " $data_output_dir/experiment_*.txt)
-JAILHOUSE_OUTPUT_FILE=$(grep_token_in_file "JAILHOUSE_OUTPUT_FILE: " $data_output_dir/experiment_*.txt)
-LINUX_OUTPUT_FILE=$(grep_token_in_file "LINUX_OUTPUT_FILE: " $data_output_dir/experiment_*.txt)
+JAILHOUSE_OUTPUT_BASENAME=$(grep_token_in_file "JAILHOUSE_OUTPUT_BASENAME: " $data_output_dir/experiment_*.txt)
+JAILHOUSE_OUTPUT_FILE="$data_output_dir/$JAILHOUSE_OUTPUT_BASENAME"
+LINUX_OUTPUT_BASENAME=$(grep_token_in_file "LINUX_OUTPUT_BASENAME: " $data_output_dir/experiment_*.txt)
+LINUX_OUTPUT_FILE="$data_output_dir/$LINUX_OUTPUT_BASENAME"
 RUN_MODE=$(grep_token_in_file "RUN_MODE_RAW: " $data_output_dir/experiment_*.txt)
 RUN_WITH_VTUNE=$(grep_token_in_file "RUN_WITH_VTUNE: " $data_output_dir/experiment_*.txt)
 THROTTLE_MODE=$(grep_token_in_file "THROTTLE_MODE_RAW: " $data_output_dir/experiment_*.txt)
 # THROTTLE_MECHANISM=$(grep_token_in_file "THROTTLE_MECHANISM_RAW: " $data_output_dir/experiment_*.txt)
 TIMESTAMP=$(grep_token_in_file "TIMESTAMP: " $data_output_dir/experiment_*.txt)
 # VTUNE_MODE=$(grep_token_in_file "VTUNE_MODE_RAW: " $data_output_dir/experiment_*.txt)
-VTUNE_OUTPUT_FILE=$(grep_token_in_file "VTUNE_OUTPUT_FILE: " $data_output_dir/experiment_*.txt)
+VTUNE_OUTPUT_BASENAME=$(grep_token_in_file "VTUNE_OUTPUT_BASENAME: " $data_output_dir/experiment_*.txt)
+VTUNE_OUTPUT_FILE="$data_output_dir/$VTUNE_OUTPUT_BASENAME"
 WORKLOAD_MODE=$(grep_token_in_file "WORKLOAD_MODE_RAW: " $data_output_dir/experiment_*.txt)
 
 post_process_data $new_output_dir $TIMESTAMP
