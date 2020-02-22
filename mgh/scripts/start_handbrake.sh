a278757031a04b4c98dc22eddd13358e320d39d9
Michael Hinton <michael.g.hinton@gmail.com>
2020-02-22 11:42:20 -0700

    Get start_handbrake.sh to work with new changes
    Move input and output files within the mgh/scripts dir

--- mgh/scripts/start_handbrake.sh
+++ mgh/scripts/start_handbrake.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 source ./common.sh
 
-start_handbrake_demo
+INPUT="/home/hintron/code/jailhouse/mgh/scripts/sources/i_am_legend.m2ts"
+OUTPUT="/home/hintron/code/jailhouse/mgh/scripts/output/handbrake-ad-hoc.out"
+HandBrakeCLI -i $INPUT -o $OUTPUT
