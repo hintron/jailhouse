2ebd2a07ff42030f60d1c91c736fb1f9e0003e90
Michael Hinton <michael.g.hinton@gmail.com>
2020-03-27 02:30:12 -0600

    Alphabetize order

--- mgh/workloads/include/workloads.h
+++ mgh/workloads/include/workloads.h
@@ -1,6 +1,6 @@
 // This is only for convenience for main(). The real prototype used by the
 // inmate is in inmates/lib/x86/include/inmate.h
 
-void *sha3_mgh(const void *, int, void *, int);
 int count_set_bits_mgh(unsigned char *, int, int);
 unsigned long long random_access_mgh(unsigned char *, int);
+void *sha3_mgh(const void *, int, void *, int);
