fb854df7f07dc332a73264f445f230ef89b55c22
Michael Hinton <michael.g.hinton@gmail.com>
2020-03-27 02:05:37 -0600

    Alphabetize order

--- inmates/lib/x86/include/inmate.h
+++ inmates/lib/x86/include/inmate.h
@@ -262,9 +262,9 @@ void pci_msi_set_vector(u16 bdf, unsigned int vector);
 void pci_msix_set_vector(u16 bdf, unsigned int vector, u32 index);
 
 // MGH: Add workloads
-int sha3_mgh(const void *, int, void *, int);
 int count_set_bits_mgh(unsigned char *, int, int);
 u64 random_access_mgh(unsigned char *, int);
+int sha3_mgh(const void *, int, void *, int);
 
 extern volatile u32 smp_num_cpus;
 extern u8 smp_cpu_ids[SMP_MAX_CPUS];
